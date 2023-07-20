8122771f61ed55a000cc95a03f4324108ec38931
YogiPradnyana <15tkj1yogipradnyana.com>
2023-07-20 09:47:12 +0800

    Menambahkan file index.html dan script.js yang sudah jadi

---
+++ script.js
@@ -0,0 +1,87 @@
+let selectedRow = null;
+
+// Show ALert
+function showAlert(message, className) {
+  const div = document.createElement('div');
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+
+  const container = document.querySelector('.container');
+  const main = document.querySelector('.main');
+  container.insertBefore(div, main);
+
+  setTimeout(() => {
+    document.querySelector('.alert').remove();
+  }, 2000);
+}
+
+// Clear Fields
+function clearFields() {
+  document.querySelector('#nama').value = '';
+  document.querySelector('#nim').value = '';
+  document.querySelector('#jurusan').value = '';
+}
+
+// Add Data
+document.querySelector('#mahasiswa-form').addEventListener('submit', () => {
+  // Get Fields
+  const nama = document.querySelector('#nama').value;
+  const nim = document.querySelector('#nim').value;
+  const jurusan = document.querySelector('#jurusan').value;
+
+  // Validated
+  if (nama == '' || nim == '' || jurusan == '') {
+    showAlert('All data must be filled in', 'danger');
+  } else {
+    if (selectedRow == null) {
+      const list = document.querySelector('#mahasiswa-list');
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+            <td>${nama}</td>
+            <td>${nim}</td>
+            <td>${jurusan}</td>
+            <td>
+                <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+                <a href="#" class="btn btn-danger btn-sm hapus">Hapus</a>
+            </td>
+            `;
+      list.appendChild(tr);
+      console.log(list);
+      showAlert('Mahasiswa has been added', 'success');
+      selectedRow = null;
+    } else {
+      selectedRow.children[0].textContent = nama;
+      selectedRow.children[1].textContent = nim;
+      selectedRow.children[2].textContent = jurusan;
+      showAlert('Mahasiswa has been edited', 'info');
+      selectedRow = null;
+    }
+  }
+  clearFields();
+});
+
+// Edit Data
+document.querySelector('#mahasiswa-list').addEventListener('click', (e) => {
+  let target = e.target;
+  selectedRow = target.parentElement.parentElement;
+  if (target.classList.contains('edit')) {
+    document.querySelector('#nama').value = selectedRow.children[0].textContent;
+    document.querySelector('#nim').value = selectedRow.children[1].textContent;
+    document.querySelector('#jurusan').value = selectedRow.children[2].textContent;
+  }
+});
+
+// Delete Data
+document.querySelector('#mahasiswa-list').addEventListener('click', (e) => {
+  let target = e.target;
+  if (target.classList.contains('hapus')) {
+    if (confirm('Apakah anda yakin?')) {
+      target.parentElement.parentElement.remove();
+      showAlert('Mahasiswa has been deleted', 'danger');
+      selectedRow = null;
+      clearFields();
+    } else {
+      selectedRow = null;
+    }
+  }
+});
